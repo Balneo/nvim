b8099860be6e5e98ab0aaa63720aa26f18c261af
Balneo <fritz.forsgren@gmail.com>
2026-04-15 20:39:11 +0200

    disable pluggins and remove unneccessary mappings

---
+++ .config/nvim-next/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+    { "mfussenegger/nvim-dap", enabled = false },
+    { "rcarriga/nvim-dap-ui", enabled = false },
+    { "leoluz/nvim-dap-go", enabled = false },
+    { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+    { "nvim-neotest/nvim-nio", enabled = false },
+}
